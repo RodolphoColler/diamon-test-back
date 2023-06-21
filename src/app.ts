648584cdad031b97bce5bcc34b3c23d1fd04d421
Rodolpho Coller <rodolpho.coller12@gmail.com>
2023-06-21 17:31:27 -0300

    connects routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import User from './routes/user';
+import Department from './routes/department';
 
 dotenv.config();
 
@@ -9,12 +11,13 @@ const app = express();
 app.use(express.json());
 
 const corsConfig = {
-  origin: ['http://localhost:3000', 'https://blizter.vercel.app'],
+  origin: ['http://localhost:5173'],
   credentials: true,
 };
 
 app.use(cors(corsConfig));
 
-// app.use('/user', router.user);
+app.use('/user', User);
+app.use('/department', Department);
 
 export default app;
